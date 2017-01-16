908f11d959ba3b0aad4acfea3ccf6bb872e80799
Benjamin LeBrun <bl.lebrun@gmail.com>
2017-01-16 00:42:52 -0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/IvanMenshykov/MoonPhase"
+	"github.com/bglebrun/WeatherGo"
+)
+
+func main() {
+	city, state := os.Args[1], os.Args[2]
+	w := WeatherGo.MakeQuery(WeatherGo.BuildURL(WeatherGo.BuildLocation(city, state)))
+	if w == nil {
+		fmt.Printf("Program Error")
+	} else {
+		fmt.Printf("Temperature: %s %s, %s, Humidity: %s", w.Temp, w.Tp, w.Weth, w.Humidity)
+	}
+
+	phase := getPhaseString()
+
+	fmt.Printf("\nMoon Phase: %s \n", phase)
+
+}
+
+func getPhaseString() string {
+	m := MoonPhase.New(time.Now())
+	return m.PhaseName()
+}
